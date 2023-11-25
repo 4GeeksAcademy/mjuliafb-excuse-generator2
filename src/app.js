031ca7038637964eeb441d23c236feaa66203cfc
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-11-25 23:02:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+const generateExcuse = () => {
+  function whoStr() {
+    // Add your code after this line
+    let whoArray = ["The dog", "My grandma", "His turtle", "My bird"];
+    let whoIndex = Math.floor(Math.random() * whoArray.length);
+    return whoArray[whoIndex];
+  }
+
+  function actionStr() {
+    // Add your code after this line
+    let actionArray = ["ate", "peed", "crushed", "broke"];
+    let actionIndex = Math.floor(Math.random() * actionArray.length);
+    return actionArray[actionIndex];
+  }
+
+  function whatStr() {
+    // Add your code after this line
+    let whatArray = ["my homework", "the keys", "the car"];
+    let whatIndex = Math.floor(Math.random() * whatArray.length);
+    return whatArray[whatIndex];
+  }
+  function whenStr() {
+    // Add your code after this line
+    let whenArray = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let whenIndex = Math.floor(Math.random() * whenArray.length);
+    return whenArray[whenIndex];
+  }
+
+  return whoStr() + " " + actionStr() + " " + whatStr() + " " + whenStr();
 };
