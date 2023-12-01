e4c05b8f5ec73eef2e10fe8d6d4b10a8301b32a7
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-12-01 18:55:32 +0000

    better js

--- src/app.js
+++ src/app.js
@@ -7,39 +7,59 @@ window.onload = function() {
   document.querySelector("#excuse").innerHTML = generateExcuse();
 };
 
+// const generateExcuse = () => {
+//   function whoStr() {
+//     // Add your code after this line
+//     let whoArray = ["The dog", "My grandma", "His turtle", "My bird"];
+//     let whoIndex = Math.floor(Math.random() * whoArray.length);
+//     return whoArray[whoIndex];
+//   }
+
+//   function actionStr() {
+//     // Add your code after this line
+//     let actionArray = ["ate", "peed", "crushed", "broke"];
+//     let actionIndex = Math.floor(Math.random() * actionArray.length);
+//     return actionArray[actionIndex];
+//   }
+
+//   function whatStr() {
+//     // Add your code after this line
+//     let whatArray = ["my homework", "the keys", "the car"];
+//     let whatIndex = Math.floor(Math.random() * whatArray.length);
+//     return whatArray[whatIndex];
+//   }
+//   function whenStr() {
+//     // Add your code after this line
+//     let whenArray = [
+//       "before the class",
+//       "right on time",
+//       "when I finished",
+//       "during my lunch",
+//       "while I was praying"
+//     ];
+//     let whenIndex = Math.floor(Math.random() * whenArray.length);
+//     return whenArray[whenIndex];
+//   }
+
+//   return whoStr() + " " + actionStr() + " " + whatStr() + " " + whenStr();
+// };
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
 const generateExcuse = () => {
-  function whoStr() {
-    // Add your code after this line
-    let whoArray = ["The dog", "My grandma", "His turtle", "My bird"];
-    let whoIndex = Math.floor(Math.random() * whoArray.length);
-    return whoArray[whoIndex];
-  }
+  let whatArray = ["my homework", "the keys", "the car"];
+  let actionArray = ["ate", "peed", "crushed", "broke"];
+  let whoArray = ["The dog", "My grandma", "His turtle", "My bird"];
+  let whenArray = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  function actionStr() {
-    // Add your code after this line
-    let actionArray = ["ate", "peed", "crushed", "broke"];
-    let actionIndex = Math.floor(Math.random() * actionArray.length);
-    return actionArray[actionIndex];
-  }
+  const whoIndex = getRandomIndex(whoArray);
+  const actionIndex = getRandomIndex(actionArray);
+  const whatIndex = getRandomIndex(whatArray);
+  const whenIndex = getRandomIndex(whenArray);
 
-  function whatStr() {
-    // Add your code after this line
-    let whatArray = ["my homework", "the keys", "the car"];
-    let whatIndex = Math.floor(Math.random() * whatArray.length);
-    return whatArray[whatIndex];
-  }
-  function whenStr() {
-    // Add your code after this line
-    let whenArray = [
-      "before the class",
-      "right on time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ];
-    let whenIndex = Math.floor(Math.random() * whenArray.length);
-    return whenArray[whenIndex];
-  }
-
-  return whoStr() + " " + actionStr() + " " + whatStr() + " " + whenStr();
+  return `${whoArray[whoIndex]} ${actionArray[actionIndex]} ${whatArray[whatIndex]} ${whenArray[whenIndex]}`;
 };
